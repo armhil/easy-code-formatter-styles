3c6048ad63469cf0ccfe76f59cb59b3bb7efedff
Mac <mac@mac-macbook-air.local>
2020-02-18 00:53:21 +0300

    A11YDark style addition

---
+++ src/themes/A11YDark.ts
@@ -0,0 +1,27 @@
+import { ITheme } from "./ITheme";
+
+export const Desert : ITheme = {
+
+	DisplayName: 'Desert',
+
+	CodeStyles: {
+		Keyword: 'color:#DCC6E0',
+		Comment: 'color:#D4D0AB',
+		Plaintext: 'color:#fff',
+		Punctuation: 'color:#fff',
+		String: 'color:#ABE338',
+		Literal: 'color:#F5AB35',
+		Type: 'color:#00E0E0',
+		Tag: 'color:#00E0E0',
+		AttributeName: 'color:#FFD700;font-weight:bold',
+		AttributeValue: 'color:#ABE338',
+		Decimal: 'color:#F5AB35',
+		NoCode: 'color:#000;background-color:none;'
+	},
+
+	// Background color
+	BackgroundStyle: 'background-color:#2b2b2b;',
+
+	// Line number background colors
+	LineNumberStyle: 'color:white'
+}
