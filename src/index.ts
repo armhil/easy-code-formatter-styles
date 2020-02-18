188316da01193ed70f1d903019577b67ce09a768
Mac <mac@mac-macbook-air.local>
2020-02-18 11:25:34 +0300

    A11YLight style addition

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Default } from './themes/Default';
 import { Desert } from './themes/Desert';
 import { Sunburst } from './themes/Sunburst';
 import { A11YDark } from './themes/A11YDark';
+import { A11YLight } from './themes/A11YLight';
 
-export default { Default, Desert, Sunburst, A11YDark };
+export default { Default, Desert, Sunburst, A11YDark, A11YLight };
