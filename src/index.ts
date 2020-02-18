70832eac7f53ac278f75ea107542d2584a0dd504
bgrabhdr <bugrahan.bahadir@gmail.com>
2020-02-18 16:16:38 +0300

    adding A11YLight Style (#2)
    * A11YLight style addition
    
    * A11YLight style addition
    
    * Update package.json
    
    Bumping the version
    
    Co-authored-by: Arman Hilmioglu <arman.hilmioglu@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Default } from './themes/Default';
 import { Desert } from './themes/Desert';
 import { Sunburst } from './themes/Sunburst';
 import { A11YDark } from './themes/A11YDark';
+import { A11YLight } from './themes/A11YLight';
 
 export default
 {
 	Default,
 	Desert,
 	Sunburst,
-	A11YDark
+	A11YDark,
+  A11Light
 };
