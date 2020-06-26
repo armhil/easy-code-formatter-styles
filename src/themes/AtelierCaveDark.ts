dbcd92c65b2fa56a41c4b5a7e64ecd8a495ecfc3
Arman Hilmioglu <arman.hilmioglu@gmail.com>
2020-06-26 17:07:16 +0100

    Adding Atelier dark

---
+++ src/themes/AtelierCaveDark.ts
@@ -0,0 +1,27 @@
+import { ITheme } from "./ITheme";
+
+export const AtelierCaveDark : ITheme = {
+
+	DisplayName: 'Atelier Cave Dark',
+
+	CodeStyles: {
+		Keyword: 'color:#955ae7',
+		Comment: 'color:#7e7887',
+		Plaintext: 'color:#333333',
+		Punctuation: 'color:#333333',
+		String: 'color:#2a9292',
+		Literal: 'color:#C07156',
+		Type: 'color:#aa573c',
+		Tag: 'color:#be4678',
+		AttributeName: 'color:#759731;font-weight:bold',
+		AttributeValue: 'color:#be4678',
+		Decimal: 'color:#aa573c',
+		NoCode: 'color:#576ddb;background-color:none;'
+	},
+
+	// Background color
+	BackgroundStyle: 'background-color:#19171c;',
+
+	// Line number background colors
+	LineNumberStyle: 'color:#AEB0B3'
+}
