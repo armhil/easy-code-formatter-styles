6b9aca9b7f1e094e9e2024e3857ed2636c9d61ba
Arman Hilmioglu <arman.hilmioglu@gmail.com>
2020-06-26 17:05:08 +0100

    Create AtelierCaveLight.ts

---
+++ src/themes/AtelierCaveLight.ts
@@ -0,0 +1,27 @@
+import { ITheme } from "./ITheme";
+
+export const AtelierCaveLight : ITheme = {
+
+	DisplayName: 'Atelier Cave Light',
+
+	CodeStyles: {
+		Keyword: 'color:#955ae7',
+		Comment: 'color:#7e7887',
+		Plaintext: 'color:#aa573c',
+		Punctuation: 'color:#8b8792',
+		String: 'color:#2a9292',
+		Literal: 'color:#C07156',
+		Type: 'color:#576ddb',
+		Tag: 'color:#be4678',
+		AttributeName: 'color:#759731;font-weight:bold',
+		AttributeValue: 'color:#be4678',
+		Decimal: 'color:#aa573c',
+		NoCode: 'color:#576ddb;background-color:none;'
+	},
+
+	// Background color
+	BackgroundStyle: 'background-color:#efecf4;',
+
+	// Line number background colors
+	LineNumberStyle: 'color:#AEB0B3'
+}
