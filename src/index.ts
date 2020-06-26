da0e35d1d005db6e284c2d1d15986f49dbf08c9f
Arman Hilmioglu <arman.hilmioglu@gmail.com>
2020-06-26 17:11:13 +0100

    Adding atelier cave styles. (#6)
    * Create AtelierCaveLight.ts
    
    * Adding Atelier dark
    
    * Update index.ts
    
    * Update package.json
    
    Bumping the package version, adding 2 styles.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Desert } from './themes/Desert';
 import { Sunburst } from './themes/Sunburst';
 import { A11YDark } from './themes/A11YDark';
 import { A11YLight } from './themes/A11YLight';
+import { AtelierCaveLight } from './themes/AtelierCaveLight';
+import { AtelierCaveDark } from './themes/AtelierCaveDark';
 
 export default
 {
@@ -10,5 +12,7 @@ export default
 	Desert : Desert,
 	Sunburst : Sunburst,
 	A11YDark : A11YDark,
-	A11YLight: A11YLight
+	A11YLight: A11YLight,
+	AtelierCaveLight: AtelierCaveLight,
+	AtelierCaveDark: AtelierCaveDark
 };
