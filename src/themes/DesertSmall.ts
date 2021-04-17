1f9061460bdf5e495c76c8c3cdc488c694f530a4
KyriakosG78 <giannoulakiss@gmail.com>
2021-04-18 02:32:27 +0300

    Add styles

--- src/themes/DesertSmall.ts
+++ src/themes/DesertSmall.ts
@@ -1,1 +1,27 @@
+import { ITheme } from "./ITheme";
 
+export const DesertSmall : ITheme = {
+
+	DisplayName: 'DesertSmall',
+
+	CodeStyles: {
+		Keyword: 'color:#0000E6;font-size:8px',
+		Comment: 'color:#969696;font-size:8px',
+		Plaintext: 'color:#fff;font-size:8px',
+		Punctuation: 'color:#fff;font-size:8px',
+		String: 'color:#CE7B00;font-size:8px',
+		Literal: 'color:#CE7B00;font-size:8px',
+		Type: 'color:#0000E6;font-size:8px',
+		Tag: 'color:#0000E6;font-size:8px',
+		AttributeName: 'color:#98fb98;font-size:8px;font-weight:bold',
+		AttributeValue: 'color:#98fb98;font-size:8px',
+		Decimal: 'color:#98fb98;font-size:8px',
+		NoCode: 'color:#000;background-color:none;'
+	},
+
+	// Background color
+	BackgroundStyle: 'background-color:#000;',
+
+	// Line number background colors
+	LineNumberStyle: 'color:AEB0B3'
+}
