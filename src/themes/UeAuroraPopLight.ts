fc6eacb58de167bddfbac6aa0d329779b6cd36ee
DearOhDeer <77930050+DearOhDeer@users.noreply.github.com>
2026-03-02 10:56:46 +0100

    Add UE Neon Candy Light and UE Aurora Pop Light themes (#50)
    * Add UE Neon Candy Light and UE Aurora Pop Light themes
    
    * Bump package version to 1.2.19

---
+++ src/themes/UeAuroraPopLight.ts
@@ -0,0 +1,26 @@
+import { ITheme } from './ITheme';
+
+export const UeAuroraPopLight: ITheme = {
+  DisplayName: 'UE Aurora Pop Light',
+  CodeStyles: {
+    Keyword: { Color: '4f46e5', FontWeight: 'bold' },     
+    Comment: { Color: '16a34a', FontStyle: 'italic' },    
+    Plaintext: { Color: '111827' },                       
+    Punctuation: { Color: '334155' },                    
+    String: { Color: 'db2777' },                          
+    Literal: { Color: '8b5cf6' },                         
+    Type: { Color: '0891b2' },                            
+    Tag: { Color: '2563eb' },                             
+    AttributeName: { Color: '059669' },                   
+    AttributeValue: { Color: 'c026d3' },                  
+    Decimal: { Color: 'f59e0b' },                        
+    NoCode: { Color: '7c3aed' }, 
+  },
+  BackgroundStyle: {
+    BackgroundColor: 'fcfcff',
+  },
+  LineNumberStyle: {
+    Color: '64748b',
+    BackgroundColor: 'fcfcff',
+  },
+};
